17d97fc3b07d252ee99838ad3d309f72a9647781
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2022-09-27 19:23:30 -0500

    updating

--- Jupyter-Workspace/.ipynb_checkpoints/grocery_list_core-checkpoint.py
+++ Jupyter-Workspace/.ipynb_checkpoints/grocery_list_core-checkpoint.py
@@ -8,42 +8,37 @@ organize her grocery shopping list.
 
 # @TODO: Create a list of groceries
 
-groceries = ["Water", "Butter", "Eggs", "Apples", "Cinnamon", "Sugar", "Milk"]
+groceries = ["Water, Butter, Eggs, Apples, Cinnamon, Sugar, Milk"]
 
 
 
 # @TODO: Find the first two items on the list
-print(groceries[:2])
+print(groceries [0:1])
 
 
 
 # @TODO: Find the last five items on the list
-print(groceries[2:])
+
 
 
 
 # @TODO: Find every other item on the list, starting from the second item
-print(groceries[1::2])
+
 
 
 
 # @TODO: Add an element to the end of the list
-groceries.append("flour")
-print(groceries)
 
 
 
 
 
 # @TODO: Changes a specified element within the list at the given index
-groceries[3] = "Gala Apples"
-print(groceries)
-print()
+
 
 
 
 
 # @TODO: Calculate how many items you have in the list
-print(len(groceries))
-print()
+
 
